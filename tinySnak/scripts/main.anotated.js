82a81f48cd73ccfcd40a4f1608d41051b4111831
Visut Hsu <visuthsu@gmail.com>
2025-01-03 22:54:29 +0700

    minor changes

--- tinySnak/scripts/main.anotated.js
+++ tinySnak/scripts/main.anotated.js
@@ -1,22 +1,21 @@
 
-let {world:W, system:S} = await import("@minecraft/server"), //import stuffs
+let {world:P, system:S} = await import("@minecraft/server"), //import stuffs
 
     // place blocks then return the position
-    b = (p,b)=>W.getDimension("overworld").setBlockType({x:p>>4,y:-60, z:p & 15}, b + "_concrete") || p,
+    b = (p,b)=>P.dimension?.setBlockType({x:p>>4,y:-60, z:p & 15}, b + "_concrete") || p,
 
     d = 1, // move direction
     h = 0, // head position
     a = 3, // apple position
-    s = 1, // score aka length
+    s = 0, // score aka length
 
     f = c=> S.runTimeout(_=>{
 
         // update direction if the new direction is valid
-        d = (d&3? d|48: d|3) & 1<< W.getPlayers()[0].selectedSlotIndex || d
-
+        d = (d&3? d|48: d|3) & 1<< P.selectedSlotIndex || d
+        
         // update head position place the apple then update the body
         c(b(h = h + (d & 17) - (d/2 & 17) & 255), "green")^b(a, "red")? s: ++s)
-
         let p = h // save head as this segment position
 
         // recursion
@@ -25,7 +24,7 @@ let {world:W, system:S} = await import("@minecraft/server"), //import stuffs
             h^a?b(p, "white"):a = Math.random() * 256 | 0) // update tails (generate apple pos / removing tail blocks)
       },4)
 
-f(_=>_) // start the loop
+f(_=>[P] = P.getPlayers()) // start the loop
 
 // note:
 // positions are in this format: 0bXxxxYyyy
